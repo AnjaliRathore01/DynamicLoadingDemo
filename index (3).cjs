39d1a8d66051aa5234db984cd1b9ee87abe4ed7e
AnjaliRathore01 <103346799+AnjaliRathore01@users.noreply.github.com>
2023-12-07 19:21:04 +0530

    demo

---
+++ index (3).cjs
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return _default;
+    }
+});
+function ImageComponent() {
+    return /*#__PURE__*/ React.createElement("h2", null, "DynamicLoading");
+}
+const _default = ImageComponent;
+
